2f1039bb7615f87ebe5d907148895538f4c54eff
AA <106406906+AlphaCoder0310@users.noreply.github.com>
2025-05-18 15:07:55 +0800

    Update ffff.py

--- ffff.py
+++ ffff.py
@@ -1,73 +1,15 @@
 {
-  "$addFields": {
-    "timeSeries": {
-      "$reduce": {
-        "input": "$timeSeries",
-        "initialValue": {
-          "cumulativeTrades": {},  // Will store {counterpartyType1: total1, counterpartyType2: total2}
-          "processedSeries": []    // Final output with cumulative data
-        },
-        "in": {
-          "$let": {
-            "vars": {
-              "currentCpType": "$$this.counterpartyType",
-              "currentTrades": "$$this.current.trades",
-              "previousTotal": {
-                "$ifNull": [
-                  {"$getField": {
-                    "field": {"$literal": "$$this.counterpartyType"},
-                    "input": "$$value.cumulativeTrades"
-                  }},
-                  0
-                ]
-              },
-              "newTotal": {
-                "$add": ["$$previousTotal", "$$currentTrades"]
-              }
-            },
-            "in": {
-              "cumulativeTrades": {
-                "$mergeObjects": [
-                  "$$value.cumulativeTrades",
-                  {
-                    "$arrayToObject": [
-                      [
-                        {
-                          "k": "$$currentCpType",
-                          "v": "$$newTotal"
-                        }
-                      ]
-                    ]
-                  }
-                ]
-              },
-              "processedSeries": {
-                "$concatArrays": [
-                  "$$value.processedSeries",
-                  [
-                    {
-                      "$mergeObjects": [
-                        "$$this",
-                        {
-                          "cumulative": {
-                            "trades": "$$newTotal"
-                          }
-                        }
-                      ]
-                    }
-                  ]
-                ]
-              }
-            }
-          }
-        }
+  "$setWindowFields": {
+    "partitionBy": {
+      "productRic": "$_id.productRic",
+      "counterpartyType": "$_id.counterpartyType"
+    },
+    "sortBy": {"_id.timeBin": 1},
+    "output": {
+      "cumulativeTrades": {
+        "$sum": "$tradeCount",
+        "window": {"documents": ["unbounded", "current"]}
       }
     }
-  }
-},
-{
-  "$project": {
-    "timeSeries": "$timeSeries.processedSeries",
-    "_id": 0
   }
 }
